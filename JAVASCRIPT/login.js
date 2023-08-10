9297883223e0ac9908ce478c2cf5a58f136a3f71
RAJARAJAN-JAYAKUMAR <jrajanraja1974@gmail.com>
2023-08-10 12:44:01 +0530

    added

---
+++ JAVASCRIPT/login.js
@@ -0,0 +1,16 @@
+a = 2
+b =  5
+c =  1
+
+if(a>b && a>c)
+{
+    document.write('a is larger')
+}
+else if(b>c)
+{
+    document.write("B is larger")
+}
+else
+{
+    document.write("c is large")
+}
